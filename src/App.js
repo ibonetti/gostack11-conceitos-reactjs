03f384d10bfcde4ae8f41e8d6bd0d76c9a40e15b
Israel Bonetti <israelbonetti@MacBook-Pro-de-Israel.local>
2020-05-21 17:00:34 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() =>{
+
+    async function loadRepositories(){
+      console.log('load');
+      const response = await api.get('repositories');
+      console.log(response);
+      setRepositories(response.data);
+    }
+
+    loadRepositories();
+  },[]);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories', {
+        url: "https://github.com/Rocketseat/unform",
+        title: 'Unform',
+        techs: ['ReactJS', 'React Native'],
+    });
+
+    if (response.data){
+      console.log(response.data);
+      setRepositories([...repositories, response.data]);
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`);
+    console.log(response);
+
+    if (response.status === 204){
+      const index = repositories.findIndex(repo => repo.id === id);
+
+      if (index >= 0){
+        const newRepositories = [...repositories];
+        newRepositories.splice(index, 1);
+        console.log(newRepositories);
+        setRepositories(newRepositories);
+      }
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repo => (
+        <li key={repo.id}>
+          {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
           </button>
         </li>
-      </ul>
-
+      ))}
+        </ul>
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
